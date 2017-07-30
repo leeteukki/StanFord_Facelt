836524e2edce7cc3f54feb3c0a1970155409ea4b
leeteukki <tklohj@naver.com>
2017-07-30 17:02:02 +0900

    FixVIew

--- Facelt/FaceView.swift
+++ Facelt/FaceView.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class FaceView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+        
+       
+        var skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        var skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
 
+        let skull = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0.0, endAngle: CGFloat(2*M_PI), clockwise: false)
+        
+        skull.lineWidth = 5.0
+        UIColor.blue.set()
+        skull.stroke()
+        
+    }
 }
+
