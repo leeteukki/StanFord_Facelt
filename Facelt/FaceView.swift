2308aec360de34966484e60efcc33fca828b07df
leeteukki <tklohj@naver.com>
2017-07-30 16:07:01 +0900

    CoCoa touch Uiview

---
+++ Facelt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Facelt
+//
+//  Created by Lena Lee on 2017. 7. 30..
+//  Copyright © 2017년 Lena Lee. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Facelt/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Facelt
+//
+//  Created by Lena Lee on 2017. 7. 30..
+//  Copyright © 2017년 Lena Lee. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+}
+
